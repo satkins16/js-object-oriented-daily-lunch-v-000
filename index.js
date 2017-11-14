7a1f642f73d3c4ce4b5c4245babb552633286b48
Scott Atkins <satkins16@gmail.com>
2017-11-14 01:16:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ class Customer {
     store.customers.push(this)
   }
 }
+
+let mealId = 0
+
+class Meal {
+  constructor(title, price) {
+    this.id = ++mealId
+    this.title = title
+    this.price = price
+
+    store.meals.push(this)
+  }
+}
